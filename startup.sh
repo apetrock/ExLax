acf927d36fa3d3bba16c868ac5be4cf4794ab32f
jdelaney <a.pet.rock@gmail.com>
2024-02-09 10:43:21 -0800

    moded startup

--- startup.sh
+++ startup.sh
@@ -2,20 +2,25 @@
 
 # Check if project location is provided as an argument
 if [ $# -eq 0 ]; then
-    echo "Please provide the project location as an argument."
-    exit 1
+    working_dir=$(dirname -- "$(readlink -f -- "$0")")
+    echo "Setting working dir to: $working_dir"
+else
+    working_dir="$1"
 fi
 
 # Navigate to the project location
+cd "$working_dir" || exit
+
+# Navigate to the project location
 original_dir=$(pwd)
-cd $1
+cd $working_dir
 
 # Run npm start-all
 npx pm2 start xlax-pm2-production.json
 # Wait for 5 seconds, this is a hack since I can't find a better way
-#sleep 3
+sleep 5
 
 # Open Chromium in full screen mode
-chromium-browser --start-fullscreen http://localhost:8080/
+chromium-browser --start-fullscreen --kiosk http://localhost:8080/
 
 cd $original_dir
